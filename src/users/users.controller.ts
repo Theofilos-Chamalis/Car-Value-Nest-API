48b75786c0f2580f463e2ab5eea31a6fc9a72b57
Theofilos Chamalis <theofxam@gmail.com>
2021-12-29 17:07:03 +0200

    Added session cookie handling in signin and signup in user controller and created the signout and whoami endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
   Query,
+  Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -21,14 +22,31 @@ import { AuthService } from './auth.service';
 export class UsersController {
   constructor(private usersService: UsersService, private authService: AuthService) {}
 
+  /** Find which user I am based on the id stored inside the encrypted cookie **/
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    const user = this.usersService.findOne(session.userId);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Get('/:id')
