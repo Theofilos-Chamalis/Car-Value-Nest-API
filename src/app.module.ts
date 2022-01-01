214d3e673b49fdd85be1a42c79aeb4eae7d98a0f
Theofilos Chamalis <theofxam@gmail.com>
2022-01-01 16:18:29 +0200

    Installed @nest/config, used the ConfigService to inject env variables in the app, changed the package.json scripts to use cross-env and created new dbs for development and test

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,27 @@ import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 /** cookie-session middleware does not support es5 imports **/
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true /** Use this only for dev, for prod make manual migrations **/,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true /** Use this only for dev, for prod make manual migrations **/,
+        };
+      },
     }),
     UsersModule,
     ReportsModule,
