8dbd58da191cb49d4e01f9825d9492defd860815
Theofilos Chamalis <theofxam@gmail.com>
2022-01-09 16:03:57 +0200

    Implemented the craete report service and controller methods and its dto

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,30 @@
+import { IsLatitude, IsLongitude, IsNumber, IsString, Max, Min } from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(new Date().getFullYear())
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1_000_000)
+  mileage: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1_000_000)
+  price: number;
+}
