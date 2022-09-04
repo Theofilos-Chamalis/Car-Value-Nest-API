dc71f9174e5a0364958c062d4c7f47f6985413e4
Theofilos Chamalis <theofxam@gmail.com>
2022-09-04 16:19:56 +0300

    Switched to the new way TypeORM and NestJS work

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,45 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 /** cookie-session middleware does not support es5 imports **/
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
+const dbConfig = {
+  synchronize: false,
+  migrations: ['migrations/*.js'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db-dev.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db-test.sqlite',
+      entities: ['**/*.entity.ts'],
+      migrationsRun: true,
+    });
+    break;
+  /** Heroku specific config**/
+  case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    break;
+  default:
+    throw new Error('Unknown environment');
+}
 
 @Module({
   imports: [
@@ -17,7 +56,7 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot(dbConfig),
     UsersModule,
     ReportsModule,
   ],
